9c494cd0e95ab40bfe1e2d8abad1c2bc9ee56850
SauravChamate <sauravchamate1@gmail.com>
2026-07-02 14:42:19 +0530

    app-pipeline_added

---
+++ app-pipeline.groovy
@@ -0,0 +1,55 @@
+pipeline{
+    agent any
+    stages{
+        stage('PULL'){
+            steps{
+            git branch: 'test', url: 'https://github.com/SauravChamate/CDEC-studentapp.git'
+            }
+        }
+    }
+
+        stage('DOCKER-FRONTEND-BUILD'){
+            steps{
+                sh '''cd frontend
+                docker build -t sauravdoc99/frontend:v1'''
+        }
+    }
+
+        stage('DOCKER-BACKEND-BUILD'){
+            steps{
+                sh '''cd backend
+                docker build -t sauravdoc99/backend:v1'''
+        }
+    }
+
+        stage('DOCKER-LOGIN'){
+            steps{
+                withCredentials([usernamePassword(credentialsId: 'Docker-cred', passwordVariable: 'password_var', usernameVariable: 'username_var')]) 
+            }
+        }
+    
+        stage ('DOCKER-PUSH') {
+                steps {
+                sh '''docker push sauravdoc99/frontend:v1 
+                        docker push sauravdoc99/backend:v1 '''
+                }
+            }
+        stage ('RM-IMAGE') {
+            steps {
+                sh 'docker rmi -f $(docker images -aq)'
+        }    
+    }
+
+        stage('EKS-ACCESS') {
+    steps {
+        sh '''
+        aws eks update-kubeconfig --region us-east-1 --name my-cluster
+        '''
+        }
+    }
+        stage ('KUBECTL-APPLY') {
+            steps{
+                sh 'kubectl apply -f simple-deploy/'
+        }
+    }
+}
